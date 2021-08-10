7f4cd93945f79c576b496c2a6e65a1f01aa74567
farrukh.yasin <farrukh.yasin@10pearls.com>
2021-08-10 17:20:41 +0500

    testworthy-restructure

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ gulp.task("js", () => {
     ])
     .pipe(sourcemaps.init())
     .pipe(concat("main.js"))
-    .pipe(uglify())
+    //.pipe(uglify())
     .pipe(sourcemaps.write(src.mapPath))
     .pipe(gulp.dest(src.distPath))
     .pipe(browsersync.reload({ stream: true }));
